2aa374c48f1bca66290765a7c58ea162908d9bde
JoeShvartsev <120992324+JoeShvartsev@users.noreply.github.com>
2023-08-22 12:54:33 +0300

    124

--- src/API/refresh.js
+++ src/API/refresh.js
@@ -1,14 +1,14 @@
 import { instance, setTokenToLocalStorage } from './Instance';
 
-export const refresh = async (token)=> {
+export const refresh = async token => {
   try {
-    console.log(token)
-    const { data } = await instance.post('users/current/',{
+    console.log(token);
+    const { data } = await instance.post('users/current', {
       headers: {
-        Authorization: `Bearer ${JSON.parse(token)}`,
+        Authorization: `Bearer ${token}`,
       },
     });
-    setTokenToLocalStorage(data.token)
+    setTokenToLocalStorage(data.token);
     return data;
   } catch (error) {
     throw error;
--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,47 +1,55 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { logOut } from "API/logOut";
-import { refresh } from "API/refresh";
-import { signIn } from "API/signIn";
-import { signUp } from "API/signUp";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { logOut } from 'API/logOut';
+import { refresh } from 'API/refresh';
+import { signIn } from 'API/signIn';
+import { signUp } from 'API/signUp';
 
-export const signUpUserThunk = createAsyncThunk('user/signUp',
-async (body, { rejectWithValue }) => {
-  try {
-    const data = await signUp(body);
-    return data
-  } catch (error) {
-    return rejectWithValue(error.message);
+export const signUpUserThunk = createAsyncThunk(
+  'user/signUp',
+  async (body, { rejectWithValue }) => {
+    try {
+      const data = await signUp(body);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
-})
+);
 
-export const signInUserThunk = createAsyncThunk('user/signIn',
-async (body, { rejectWithValue }) => {
-  try {
-    const data = await signIn(body);
-    return data
-  } catch (error) {
-    return rejectWithValue(error.message);
+export const signInUserThunk = createAsyncThunk(
+  'user/signIn',
+  async (body, { rejectWithValue }) => {
+    try {
+      const data = await signIn(body);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
-})
+);
 
-export const logOutUserThunk = createAsyncThunk('user/logOut',
-async (token, { rejectWithValue }) => {
-  try {
-    const data = await logOut(token);
-    return data
-  } catch (error) {
-    return rejectWithValue(error.message);
+export const logOutUserThunk = createAsyncThunk(
+  'user/logOut',
+  async (token, { rejectWithValue }) => {
+    try {
+      const data = await logOut(token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
-})
+);
 
-export const refreshUserThunk = createAsyncThunk('user/refresh',
-async (_, { rejectWithValue }) => {
-  const token = localStorage.getItem('token');
-  try {
-    const data = await refresh(token);
-    console.log(data)
-    return data
-  } catch (error) {
-    return rejectWithValue(error.message);
+export const refreshUserThunk = createAsyncThunk(
+  'user/refresh',
+  async (_, { rejectWithValue }) => {
+    const token = localStorage.getItem('token');
+    try {
+      const data = await refresh(token);
+      console.log(data);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
-})
+);
